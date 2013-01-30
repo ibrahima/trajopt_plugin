81884271c77c2925bd43c21475cb5eee34dc36a3
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2013-01-29 22:48:46 -0800

    Refactor script to make it slightly more useful.

--- src/test_move_group.py
+++ src/test_move_group.py
@@ -14,13 +14,11 @@ import IPython
 from tf.transformations import *
 import time
 
-def test():
-    client = actionlib.SimpleActionClient('move_group', MoveGroupAction)
+
+def build_motion_plan_request(pos, quat, arm="right"):
     m = MoveGroupGoal()
-    print "Waiting for server"
-    client.wait_for_server()
-    print "Connected to actionserver"
-    m.request.group_name = "right_arm"
+    m.request.group_name = "%s_arm" % arm
+    target_link = "%s_wrist_roll_link" % arm[0]
     m.request.start_state.joint_state.name = ['br_caster_rotation_joint', 'br_caster_l_wheel_joint', 'br_caster_r_wheel_joint', 'torso_lift_joint', 'head_pan_joint', 'head_tilt_joint', 'laser_tilt_mount_joint', 'r_shoulder_pan_joint', 'r_shoulder_lift_joint', 'r_upper_arm_roll_joint', 'r_elbow_flex_joint', 'r_forearm_roll_joint', 'r_wrist_flex_joint', 'r_wrist_roll_joint', 'r_gripper_motor_slider_joint', 'r_gripper_motor_screw_joint', 'r_gripper_l_finger_joint', 'r_gripper_l_finger_tip_joint', 'r_gripper_r_finger_joint', 'r_gripper_r_finger_tip_joint', 'r_gripper_joint', 'l_shoulder_pan_joint', 'l_shoulder_lift_joint', 'l_upper_arm_roll_joint', 'l_elbow_flex_joint', 'l_forearm_roll_joint', 'l_wrist_flex_joint', 'l_wrist_roll_joint', 'l_gripper_motor_slider_joint', 'l_gripper_motor_screw_joint', 'l_gripper_l_finger_joint', 'l_gripper_l_finger_tip_joint', 'l_gripper_r_finger_joint', 'l_gripper_r_finger_tip_joint', 'l_gripper_joint', 'torso_lift_motor_screw_joint', 'fl_caster_rotation_joint', 'fl_caster_l_wheel_joint', 'fl_caster_r_wheel_joint', 'fr_caster_rotation_joint', 'fr_caster_l_wheel_joint', 'fr_caster_r_wheel_joint', 'bl_caster_rotation_joint', 'bl_caster_l_wheel_joint', 'bl_caster_r_wheel_joint']
     m.request.start_state.joint_state.position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.657967, 0.888673, -1.4311, -1.073419, -0.705232, -1.107079, 2.806742, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.848628, 0.7797, 1.396294, -0.828274, 0.687905, -1.518703, 0.394348, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
@@ -32,12 +30,10 @@ def test():
     m.request.start_state.multi_dof_joint_state.poses = [ base_pose ]
 
     pc = PositionConstraint()
-    pc.link_name = "r_wrist_roll_link"
+    pc.link_name = target_link
     pc.header.frame_id = 'odom_combined'
     pose = Pose()
-    pose.position.x = 0.5
-    pose.position.y = -0.1
-    pose.position.z = 0.798
+    pose.position = pos
 
     pc.constraint_region.primitive_poses = [pose]
     
@@ -47,17 +43,30 @@ def test():
     pc.constraint_region.primitives = [ sphere ]
     
     oc = OrientationConstraint()
-    oc.link_name = "r_wrist_roll_link"
+    oc.link_name = target_link
     oc.header.frame_id = 'odom_combined'
-    oc.orientation.x = 0
-    oc.orientation.y = 0
-    oc.orientation.z = 0
-    oc.orientation.w = 1
+    oc.orientation = quat
     c = Constraints()
     c.position_constraints = [ pc ]
     c.orientation_constraints = [ oc ]
     m.request.goal_constraints = [ c ]
+    
+    return m
 
+    
+def test():
+    client = actionlib.SimpleActionClient('move_group', MoveGroupAction)
+    
+    print "Waiting for server"
+    client.wait_for_server()
+    print "Connected to actionserver"
+    p = Point()
+    p.x = .5
+    p.y = -.01
+    p.z = 0.94
+    q = Quaternion()
+    q.w = 1
+    m = build_motion_plan_request(p, q)
     client.send_goal(m)
     t1 = time.time()
     client.wait_for_result()
